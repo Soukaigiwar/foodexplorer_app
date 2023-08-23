0845f312d4fd366b9b6d36d7c3605427da93860c
Sergio Mello <54086765+Soukaigiwar@users.noreply.github.com>
2023-08-23 16:19:54 +0100

    create sign in and sign out initial html structure

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,5 +1,18 @@
 export function SignIn() {
     return (
-        <h1>SignIn</h1>
+        <form>
+            <div>
+                <img src="" alt="logomarca do food explorer" />
+                <h1>food explorer</h1>
+            </div>
+            <div>
+                <label htmlFor="text_email">Email</label>
+                <input type="email" placeholder="Exemplo: exemplo@exemplo.com.br" />
+                <label htmlFor="text_password">Senha</label>
+                <input type="password" placeholder="No mínimo 6 caracteres" />
+                <button>Entrar</button>
+                <a href="/register">Criar uma conta</a>
+            </div>
+        </form>
     );
 };
--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,5 +1,20 @@
 export function SignUp() {
     return (
-        <h1>SignUp</h1>
+        <form>
+            <div>
+                <img src="" alt="logomarca do food explorer" />
+                <h1>food explorer</h1>
+            </div>
+            <div>
+                <label htmlFor="text_name">Seu nome</label>
+                <input type="text" placeholder="Exemplo: Maria da Silva" />
+                <label htmlFor="text_email">Email</label>
+                <input type="email" placeholder="Exemplo: exemplo@exemplo.com.br" />
+                <label htmlFor="text_password">Senha</label>
+                <input type="password" placeholder="No mínimo 6 caracteres" />
+                <button>Criar conta</button>
+                <a href="/">Já tenho uma conta</a>
+            </div>
+        </form>
     );
 };
