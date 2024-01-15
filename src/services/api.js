50f9ea985c6d58a9bf54196235a98bb73fd58e7f
Sergio Mello <54086765+Soukaigiwar@users.noreply.github.com>
2024-01-15 17:28:52 +0000

    enviroment test 3

--- src/services/api.js
+++ src/services/api.js
@@ -1,17 +1,5 @@
 import axios from "axios";
 
-export let api;
-const enviroment = import.meta.env.DEV;
-
-console.log(import.meta.env.DEV);
-console.log(enviroment);
-
-if (enviroment) {
-    api = axios.create({
-        baseURL: "http://localhost:3333",
-    });
-} else {
-    api = axios.create({
-        baseURL: "https://sergiomello-foodexplorer-api.onrender.com",
-    });
-}
+export const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL
+});
