3d20fbafdbed91301b74c25d23a3974b25c9d6ff
Sergio Mello <54086765+Soukaigiwar@users.noreply.github.com>
2024-01-29 01:44:12 +0000

    refactor OrderSummary change table to div

--- src/pages/OrderSummary/index.jsx
+++ src/pages/OrderSummary/index.jsx
@@ -27,64 +27,103 @@ export function OrderSummary() {
         <Container>
             <Header />
             <Content>
-                <h2>Histórico de Pedidos</h2>
-                <div>
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>Status</th>
-                                <th>Código</th>
-                                <th>Detalhamento</th>
-                                <th>Data e Hora</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>
-                                    <div>
-                                        <img src={bullet_red} alt="Pendente" />
-                                        <p>Pendente</p>
-                                    </div>
-                                </td>
-                                <td>0000052</td>
-                                <td>1 x Salada Radish, 1 x Torradas de Parma, 1 x Chá de Canela, 1 x Suco de Maracujá</td>
-                                <td>20/05 às 18h00</td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <div>
-                                        <img src={bullet_yellow} alt="Pendente" />
-                                        <p>Preparando</p>
-                                    </div>
-                                </td>
-                                <td>00000003</td>
-                                <td>1 x Salada Radish, 1 x Torradas de Parma, 1 x Chá de Canela, 1 x Suco de Maracujá</td>
-                                <td>20/05 às 18h00</td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <div>
-                                        <img src={bullet_green} alt="Pendente" />
-                                        <p>Entregue</p>
-                                    </div>
-                                </td>
-                                <td>0000021</td>
-                                <td>1 x Salada Radish, 1 x Torradas de Parma, 1 x Chá de Canela, 1 x Suco de Maracujá</td>
-                                <td>20/05 às 18h00</td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <div>
-                                        <img src={bullet_green} alt="Pendente" />
-                                        <p>Entregue</p>
-                                    </div>
-                                </td>
-                                <td>0000006</td>
-                                <td>1 x Salada Radish, 1 x Torradas de Parma, 1 x Chá de Canela, 1 x Suco de Maracujá</td>
-                                <td>20/05 às 18h00</td>
-                            </tr>
-                        </tbody>
-                    </table>
+                
+                <h2><span>Histórico de </span>Pedidos</h2>
+                <div id="item_list">
+                    <div id="head">
+                        <div id="head_status">Status</div>
+                        <div id="head_codigo">Código</div>
+                        <div id="head_detalhamento">Detalhamento</div>
+                        <div id="head_data_hora">Data e Hora</div>
+                    </div>
+                    <div className="lines">
+                        <div className="status">
+                            <img src={bullet_green} alt="Pendente" />
+                            <p>Entregue</p>
+                        </div>
+                        <div className="codigo">
+                            <p>0000006</p>
+                        </div>
+                        <div className="detalhamento">
+                            <p>1 x Salada Radish, 1 x Torradas de Parma, 1 x Chá de Canela, 1 x Suco de Maracujá</p>
+                        </div>
+                        <div className="data_hora">
+                            <p>20/05 às 18h00</p>
+                        </div>
+                    </div>
+                    <div className="lines">
+                        <div className="status">
+                            <img src={bullet_green} alt="Pendente" />
+                            <p>Entregue</p>
+                        </div>
+                        <div className="codigo">
+                            <p>0000006</p>
+                        </div>
+                        <div className="detalhamento">
+                            <p>1 x Salada Radish, 1 x Torradas de Parma, 1 x Chá de Canela, 1 x Suco de Maracujá</p>
+                        </div>
+                        <div className="data_hora">
+                            <p>20/05 às 18h00</p>
+                        </div>
+                    </div>
+                    <div className="lines">
+                        <div className="status">
+                            <img src={bullet_green} alt="Pendente" />
+                            <p>Entregue</p>
+                        </div>
+                        <div className="codigo">
+                            <p>0000006</p>
+                        </div>
+                        <div className="detalhamento">
+                            <p>1 x Salada Radish, 1 x Torradas de Parma, 1 x Chá de Canela, 1 x Suco de Maracujá</p>
+                        </div>
+                        <div className="data_hora">
+                            <p>20/05 às 18h00</p>
+                        </div>
+                    </div>
+                    <div className="lines">
+                        <div className="status">
+                            <img src={bullet_green} alt="Pendente" />
+                            <p>Entregue</p>
+                        </div>
+                        <div className="codigo">
+                            <p>0000006</p>
+                        </div>
+                        <div className="detalhamento">
+                            <p>1 x Salada Radish, 1 x Torradas de Parma, 1 x Chá de Canela, 1 x Suco de Maracujá</p>
+                        </div>
+                        <div className="data_hora">
+                            <p>20/05 às 18h00</p>
+                        </div>
+                    </div>
+                    <div className="lines">
+                        <div className="status">
+                            <img src={bullet_green} alt="Pendente" />
+                            <p>Entregue</p>
+                        </div>
+                        <div className="codigo">
+                            <p>0000006</p>
+                        </div>
+                        <div className="detalhamento">
+                            <p>1 x Salada Radish, 1 x Torradas de Parma, 1 x Chá de Canela, 1 x Suco de Maracujá</p>
+                        </div>
+                        <div className="data_hora">
+                            <p>20/05 às 18h00</p>
+                        </div>
+                    </div>
+
+                    {/* 
+                    <tr>
+                        <td>
+                            <div>
+                                <img src={bullet_green} alt="Pendente" />
+                                <p>Entregue</p>
+                            </div>
+                        </td>
+                        <td>0000006</td>
+                        <td>1 x Salada Radish, 1 x Torradas de Parma, 1 x Chá de Canela, 1 x Suco de Maracujá</td>
+                        <td>20/05 às 18h00</td>
+                    </tr> */}
                 </div>
             </Content>
             <Footer />
--- src/pages/OrderSummary/styles.js
+++ src/pages/OrderSummary/styles.js
@@ -2,83 +2,183 @@ import styled from "styled-components";
 import { devices } from "../../styles/media";
 
 export const Container = styled.div`
+    display: grid;
+    grid-template-rows: 104px 1fr 77px;
+    grid-template-areas:
+        "header"
+        "content"
+        "footer";
 
-display: grid;
-grid-template-rows: 104px 1fr 77px;
-grid-template-areas: 
-    "header"
-    "content"
-    "footer";
+    height: 100vh;
 
-height: 100vh;
-
-@media (${devices.desktop}) {
-
-}`;
+    @media (${devices.desktop}) {
+    }
+`;
 
 export const Content = styled.div`
     grid-area: content;
-    
+
+
+    margin: 0 3.5rem;
+
+    #item_list {
+        display: flex;
+        flex-direction: column;
+        font-family: "Roboto";
+        color: ${({ theme }) => theme.COLORS.FG_300};
+        gap: 1.7rem;
+    }
+
+    #head {
+        display: none;
+    }
+
+    .lines {
+        display: grid;
+        grid-template-areas:
+            "codigo status data_hora"
+            "detalhamento detalhamento detalhamento";
+        font-size: 1.4rem;
+        font-weight: 400;
+        line-height: 160%;
+        border: 2px solid ${({ theme }) => theme.COLORS.BG_1000};
+        border-radius: 8px;
+        padding: 1.8rem 2rem;
+        /* height: 20.4rem; */
+    }
+
+    .codigo {
+        grid-area: codigo;
+        margin-right: 3.1rem;
+    }
+
+    .status {
+        grid-area: status;
+
+        display: flex;
+
+        img {
+            margin-right: 0.8rem;
+        }
+    }
+
+    .detalhamento {
+        grid-area: detalhamento;
+        margin-top: 1.6rem;
+    }
+
+    .data_hora {
+        grid-area: data_hora;
+        white-space: nowrap;
+    }
+
     @media (${devices.desktop}) {
         grid-area: content;
-        width: 113.2rem;
         margin: 0 auto;
-        
-        h2 {
-            margin-top: 3.43rem;
-            margin-bottom: 2.62rem;
-            color: var(--Light-Light-300, ${({ theme }) => theme.COLORS.FG_300});
-            font-family: 'Poppins';
-            font-size: 32px;
-            font-style: normal;
-            font-weight: 500;
-            line-height: 140%;
+
+        #item_list {
+            font-family: "Roboto";
+            color: ${({ theme }) => theme.COLORS.FG_300};
+            gap: 0;
         }
-        
-        table, tr, th {
-            border: 1px solid ${({ theme }) => theme.COLORS.BG_1000};
-            border-spacing: 0;
-            color: ${({ theme }) => theme.COLORS.FG_300};        
+
+        #head {
+            font-style: Roboto;
+            font-size: 1.4rem;
+            display: flex;
+            flex-direction: row;
+            
+            border-top-left-radius: 0.8rem;
+            border-top-right-radius: 0.8rem;
         }
-        
-        tr {
-            th {
-                padding: 2.1rem 2.4rem;
-                font-size: 1.4rem;
-                font-weight: 700;
-                text-align: start;
-            }
-            th:nth-child(1) {
-                width: 15rem;
-            }
+
+        #head_status,
+        #head_codigo,
+        #head_detalhamento,
+        #head_data_hora {
+            padding: 2.1rem 2.4rem;
+            border: 2px solid ${({ theme }) => theme.COLORS.BG_1000};
+        }
+
+        #head_status {
+            width: 15.2rem;
+            border-top-left-radius: 0.8rem;
+            border-top-right-radius: 0;
+            border-right: none;
+        }
+
+        #head_codigo {
+            width: 15.2rem;
+            border-right: none;
+        }
+
+        #head_detalhamento {
+            width: 60rem;
+            border-right: none;
+        }
+
+        #head_data_hora {
+            width: 22rem;
+            border-top-right-radius: 0.8rem;
+        }
+
+        .lines {
+            display: grid;
+            align-items: center;
+            grid-template-areas: "status codigo detalhamento data_hora";
+            font-size: 1.4rem;
+            font-weight: 400;
+            line-height: 160%;
+            border: none;
+            padding: 0;
             
-            th:nth-child(2) {
-                width: 15rem;
-            }
-            
-            th:nth-child(3) {
-                width: 67rem;
-            }
-    
-            th:nth-child(4) {
-                width: 15rem;
-            }
-            
-            td {
-                border: 1px solid ${({ theme }) => theme.COLORS.BG_1000};
-                font-size: 1.4rem;
-                font-weight: 400;
-                padding: 16px 24px;
-                
-    
-                > div {
-                    display: flex;
-                    align-items: center;
-    
-                    img {
-                        margin-right: 0.8rem;
-                    }
-                }
+            /* padding: 1.8rem 2rem; */
+            /* height: 20.4rem; */
+        }
+
+        .codigo,
+        .status,
+        .detalhamento,
+        .data_hora {
+            border: 2px solid ${({ theme }) => theme.COLORS.BG_1000};
+            padding: 2rem;
+            margin-top: 0;
+            border-top: 0;
+        }
+
+        .codigo {
+            grid-area: codigo;
+            border-right: 0;
+            margin-right: 0;
+            width: 15.2rem;
+        }
+
+        .status {
+            grid-area: status;
+
+            display: flex;
+            align-items: center;
+            width: 15.2rem;
+
+            border-right: 0;
+
+            img {
+                width: 1rem;
+                margin-right: 0.8rem;
             }
         }
-    }`;
+
+        .detalhamento {
+            grid-area: detalhamento;
+            border-right: 0;
+            width: 60rem;
+
+        }
+
+        .data_hora {
+            grid-area: data_hora;
+            white-space: nowrap;
+            width: 22rem;
+        }
+    }
+`;
